5c22cc1cb20879b2c6d122072531aa032d91be6b
vineetha19 <42667307+vineetha19@users.noreply.github.com>
2018-10-08 04:08:42 -0700

    alarm

---
+++ app/src/main/java/com/example/admin/alarm/AlarmReceiver.java
@@ -0,0 +1,34 @@
+package com.example.admin.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    String TAG = "AlarmReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction() != null && context != null) {
+            if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
+                Log.d(TAG, "onReceive: BOOT_COMPLETED");
+                LocalData localData = new LocalData(context);
+                NotificationScheduler.setReminder(context, AlarmReceiver.class, localData.get_hour(), localData.get_min());
+                return;
+            }
+        }
+        Log.d(TAG, "onReceive: ");
+
+        NotificationScheduler.showNotification(context, MainActivity.class,
+                "You have 5 unwatched videos", "Watch them now?");
+
+    }
+}
+
+
+
+
+
